daa99100e93b884447e716086cfa7aae59dce9b2
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2025-01-16 17:21:49 +0600

    Add token verification middleware to secure routes

--- index.js
+++ index.js
@@ -47,6 +47,21 @@ async function run() {
       res.send({ token });
     });
 
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if(!req.headers.authorization) {
+        return res.status(401).send({message: 'unauthorized access'})
+      }
+      const token  = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+
     // save a new user in db
     app.post('/users/:email', async (req, res) => {
       const { email } = req.params;
